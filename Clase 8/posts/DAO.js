10cd02b7a0ec2d964f9fd5089e81d16ca744a900
sledesma <sebas@Scorpion.sebas.com>
2021-07-29 15:54:19 -0300

    Mitad de la clase

--- Clase 8/posts/DAO.js
+++ Clase 8/posts/DAO.js
@@ -3,70 +3,104 @@
 
 // Data - Operaciones directas al origen de datos
 const jsonplaceholderDataSource = {
+	endpoint: "https://jsonplaceholder.typicode.com/",
 
-  endpoint: "https://jsonplaceholder.typicode.com/",
+	async getAll(resource) {
+		const res = await fetch(`${this.endpoint}${resource}`);
+		const data = await res.json();
+		return data;
+	},
 
-  async getAll(resource) {
-    const res = await fetch(`${this.endpoint}${resource}`);
-    const data = await res.json()
-    return data;
-  },
+	async getOne(resource, id) {
+		const res = await fetch(`${this.endpoint}${resource}/${id}`);
+		const data = await res.json();
+		return data;
+	},
 
-  async getOne(resource, id) {
-    const res = await fetch(`${this.endpoint}${resource}/${id}`);
-    const data = await res.json()
-    return data;
-  }
-
-}
+	async create(resource, data) {
+		const res = await fetch(`${this.endpoint}${resource}`, {
+			method: "POST",
+      body: data
+		});
+		return res.status;
+	},
+};
 
 // Adapter
 const postsRespository = (dataSource) => ({
-  dataSource: dataSource,
+	dataSource: dataSource,
 
-  async getAll() {
-    const data = await dataSource.getAll('posts')
-    return data.map(item => postObject(item))
-  },
+	async getAll() {
+		const data = await dataSource.getAll("posts");
+		return data.map((item) => postObject(item));
+	},
 
-  async getOne(id) {
-    const data = await dataSource.getOne('posts', id)
-    return postObject(data)
+	async getOne(id) {
+		const data = await dataSource.getOne("posts", id);
+		return postObject(data);
+	},
+
+  async create(data) {
+    const body = postObject(data);
+    console.log(body)
+    const status = await dataSource.create('posts', body);
+    return status;
   }
-})
+});
 // Object - Un objeto que representa el recurso
 const postObject = (postData) => ({
-  get userId() { return postData.userId },
-  get id() { return postData.id },
-  get title() { return postData.title },
-  get body() { return postData.body }
+	get userId() {
+		return postData.userId;
+	},
+	get id() {
+		return postData.id;
+	},
+	get title() {
+		return postData.title;
+	},
+	get body() {
+		return postData.body;
+	},
 });
-
-
 
 // Adapter
 const usersRespository = (dataSource) => ({
-  dataSource: dataSource,
+	dataSource: dataSource,
 
-  async getAll() {
-    const data = await dataSource.getAll('users')
-    return data.map(item => userObject(item))
-  },
+	async getAll() {
+		const data = await dataSource.getAll("users");
+		return data.map((item) => userObject(item));
+	},
 
-  async getOne(id) {
-    const data = await dataSource.getOne('users', id)
-    return userObject(data)
-  }
-})
+	async getOne(id) {
+		const data = await dataSource.getOne("users", id);
+		return userObject(data);
+	},
+});
 // Object - Un objeto que representa el recurso
 const userObject = (userData) => ({
-  get id() { return userData.id },
-  get name() { return userData.name },
-  get username() { return userData.username },
-  get email() { return userData.email },
-  get address() { return userData.address },
-  get phone() { return userData.phone },
-  get website() { return userData.website },
-  get company() { return userData.company }
+	get id() {
+		return userData.id;
+	},
+	get name() {
+		return userData.name;
+	},
+	get username() {
+		return userData.username;
+	},
+	get email() {
+		return userData.email;
+	},
+	get address() {
+		return userData.address;
+	},
+	get phone() {
+		return userData.phone;
+	},
+	get website() {
+		return userData.website;
+	},
+	get company() {
+		return userData.company;
+	},
 });
-
