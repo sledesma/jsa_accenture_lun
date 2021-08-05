1ea77ead8cca2ecaf7ce0331d7781d6c8ffae88f
sledesma <sebas@Scorpion.sebas.com>
2021-08-05 14:34:19 -0300

    Patron modulo :: 1

---
+++ Clase 10/Module.js
@@ -0,0 +1,20 @@
+/**
+ * Module = Encapsulamiento
+ * 
+ * Problema que resuelve: Necesito ocultar detalles de la implementación (private)
+ * Estrategia que ofrece: En JavaScript, podemos usar funciones y el ámbito para encapsular cosas
+**/
+
+const Numeros = (function(){
+
+  // Una función que se llama inmediatamente
+  // Dentro de una función (autoinvocada), todo es privado
+
+  const obtenerNumero = () => 23;
+
+  // La parte pública será el retorno de esta función
+  return {
+    numero: obtenerNumero()
+  }
+
+})();
