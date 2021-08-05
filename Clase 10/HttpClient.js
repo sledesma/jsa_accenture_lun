25f1a1faa1c996a0149ea0f9806df1b9cbbf61ae
sledesma <sebas@Scorpion.sebas.com>
2021-08-05 15:20:18 -0300

    Singleton

---
+++ Clase 10/HttpClient.js
@@ -0,0 +1,40 @@
+const HttpClient = (function(){
+
+  const
+    API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts/',
+    GET_ALL_ID = 'Client_GetAll'
+
+  let currentInstance = null;
+
+  class __Client {
+    constructor() {
+      this.prevResponse = {
+        id: '',
+        data: {}
+      };
+      console.log('Construyendo el Cliente...');
+    }
+
+    async getAll() {
+      if(this.prevResponse.id != GET_ALL_ID) {
+        console.log('Solicitando todos los datos desde la red...');
+        const res = await fetch(API_ENDPOINT);
+        const data = await res.json();
+        this.prevResponse = {
+          id: GET_ALL_ID,
+          data: data
+        };
+      }
+      return this.prevResponse;
+    }
+  }
+
+  function getInstance() {
+    if(currentInstance == null) {
+      currentInstance = new __Client();
+    }
+    return currentInstance;
+  }
+
+  return getInstance();
+})();
